a608292d4e363b3890bb984f9e5abe67c4dfd35b
AlexEpu <65967586+AlexEpu@users.noreply.github.com>
2021-03-08 11:10:50 +0200

    all

---
+++ src/todosData.js
@@ -0,0 +1,41 @@
+/** @format */
+
+const todosData = [
+	{
+		id: 1,
+		text: 'Take out the trash',
+		completed: false
+	},
+	{
+		id: 2,
+		text: 'Grocery shopping',
+		completed: false
+	},
+	{
+		id: 3,
+		text: 'Clean gecko tank',
+		completed: false
+	},
+	{
+		id: 4,
+		text: 'Mow lawn',
+		completed: false
+	},
+	{
+		id: 5,
+		text: 'Catch up on Arrested Development',
+		completed: false
+	},
+	{
+		id: 6,
+		text: 'Todo Item 6',
+		completed: false
+	},
+	{
+		id: 7,
+		text: 'Todo Item 7',
+		completed: false
+	}
+];
+
+export default todosData;
